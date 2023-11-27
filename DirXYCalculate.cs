65e4cd918764a66d58d9b2d3801bad495d0b412b
SheepYhangCN <13538773578@163.com>
2023-11-27 19:33:57 +0800

    i am stupid

--- DirXYCalculate.cs
+++ DirXYCalculate.cs
@@ -7,15 +7,15 @@ namespace SheepYhangCN.DirXYCalculate
     {
         public static double PointDirection(double x1,double y1,double x2,double y2)
         {
-            return double.DegreesToRadians(Math.Atan2(x2-x1,y2-y1));
+            return double.RadiansToDegrees(Math.Atan2(x2-x1,y2-y1));
         }
         public static double LengthDirX(double angle,double length)
         {
-            return Math.Sin(double.RadiansToDegrees(angle))*length;
+            return Math.Sin(double.DegreesToRadians(angle))*length;
         }
         public static double LengthDirY(double angle,double length)
         {
-            return Math.Cos(double.RadiansToDegrees(angle))*length;
+            return Math.Cos(double.DegreesToRadians(angle))*length;
         }
     }
 }
